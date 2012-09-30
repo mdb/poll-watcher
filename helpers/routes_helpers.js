6e3f63cca45a147fa8b5a1527297d374edb81303
Mike Ball <mikedball@gmail.com>
2012-09-30 13:16:56 -0400

    Moved helper functions to routes_helpers

---
+++ helpers/routes_helpers.js
@@ -0,0 +1,134 @@
+var states = require('../states');
+var pollster = require('pollster');
+
+// helpers
+exports.inThreeChunks = function(array) {
+  var splitArr = [];
+  var subListLength = Math.ceil(array.length/3);
+  var i;
+
+  for (i=0; i<subListLength; i++) {
+    var sublist = array.slice(i*subListLength, i*subListLength+subListLength);
+
+    if (sublist.length > 0) {
+      splitArr.push(array.slice(i*subListLength, i*subListLength+subListLength));
+    }
+  }
+  
+  return splitArr;
+};
+
+exports.getStateAbbrev = function(stateStr) {
+  var state = stateStr.toLowerCase();
+  var statesLength = states.length;
+  var i;
+
+  for (i=0; i<statesLength; i++) {
+    if (state === states[i].name.toLowerCase() || state === states[i].abbr.toLowerCase()) {
+      return states[i].abbr.toLowerCase();
+    }
+  }
+  return false;
+};
+
+exports.getFormattedDate = function(dateStr) {
+  var date = new Date(dateStr);
+  var month = date.getMonth();
+  var day = date.getDate();
+  var year = date.getFullYear();
+  var hours = date.getHours();
+  var mins = date.getMinutes();
+  
+  return month + '/' + day + '/' + year;// + ' ' + hours + ':' + mins;
+};
+
+exports.getPollData = function(params, callback) {
+  pollster.charts(params, function(resp) {
+    var data = [];
+    var respLength = resp.length;
+    var i;
+
+    for (i=0; i<respLength; i++) {
+      var item = {};
+      var choiceLength = resp[i].estimates.length;
+      var j;
+
+      if (resp[i].estimates.length > 0 && !exports.isPrimary(resp[i])) {
+        item.title = resp[i].title;
+        item.url = resp[i].url;
+        item.raw_last_updated = resp[i].last_updated;
+        item.formatted_last_updated = exports.getFormattedDate(item.raw_last_updated);
+        item.slug = resp[i].slug;
+        item.estimates = exports.getFormattedChoices(resp[i].estimates);
+        data.push(item);
+      }
+    }
+
+    callback(data);
+  });
+};
+
+exports.order = function(a, b, desc) {
+  return desc ? b.value - a.value : a.value - b.value;
+};
+
+exports.descendingEstimates = function(a, b) {
+  return exports.order(a, b, true);
+};
+
+exports.isPrimary = function(chart) {
+  var title = chart.title.toLowerCase();
+
+  return title.indexOf('primary') !== -1
+};
+
+exports.getFormattedChoices = function(choices) {
+  var i;
+  var choiceLength = choices.length;
+  var choiceArray = [];
+  
+  for (i=0; i<choiceLength; i++) {
+    choiceArray.push(exports.getFormattedChoice(choices[i]));
+  }
+
+  return choiceArray.sort(exports.descendingEstimates);
+};
+
+exports.getFormattedChoice = function(choice) {
+  var choiceObj = {};
+
+  choiceObj.formatted_name = exports.getFormattedName(choice);
+  choiceObj.value = choice.value;
+  choiceObj.integer_value = Math.ceil(choice.value);
+  choiceObj.party = exports.getFormattedParty(choice.party);
+  choiceObj.lower_party = choiceObj.party !== null ? choiceObj.party.toLowerCase() : 'null';
+
+  return choiceObj;
+};
+
+exports.getFormattedParty = function(party) {
+  var partyName;
+  var lowerParty = party !== null ? party.toLowerCase() : '';
+
+  if (lowerParty.indexOf('dem') !== -1) {
+    partyName = "Democrat";
+  } else if (lowerParty.indexOf('rep') !== -1) {
+    partyName = "Republican";
+  } else {
+    partyName = party;
+  }
+
+  return partyName;
+};
+
+exports.getFormattedName = function(choice) {
+  var formattedName;
+
+  if (choice.first_name && choice.last_name) {
+    formattedName = choice.first_name + ' ' + choice.last_name;
+  } else {
+    formattedName = choice.choice;
+  }
+
+  return formattedName;
+};
--- routes.js
+++ routes.js
@@ -1,19 +1,20 @@
 var pollster = require('pollster');
 var title = "2012 PollWatcher";
 var states = require('./states');
+var helpers = require('./helpers/routes_helpers');
 
 module.exports = function(app) {
   app.get('/', function(req, res){
     console.log(__dirname)
     res.render('index', {
       title: title,
-      states: inThreeChunks(states)
+      states: helpers.inThreeChunks(states)
     });
   });
 
   app.post('/', function(req, res){
     console.log(__dirname)
-    res.redirect('/state/' + getStateAbbrev(req.param('state')));
+    res.redirect('/state/' + helpers.getStateAbbrev(req.param('state')));
   });
 
   app.get('/state', function(req, res){
@@ -23,143 +24,11 @@ module.exports = function(app) {
 
   app.get('/state/:name', function(req, res){
     console.log(__dirname);
-    getPollData({state: req.param('name')}, function (data) {
+    helpers.getPollData({state: req.param('name')}, function (data) {
       res.render('state', {
         title: title,
         polls: data 
       });
     });
   });
-};
-
-// helpers
-var inThreeChunks = function(array) {
-  var splitArr = [];
-  var subListLength = Math.ceil(array.length/3);
-  var i;
-
-  for (i=0; i<subListLength; i++) {
-    var sublist = array.slice(i*subListLength, i*subListLength+subListLength);
-
-    if (sublist.length > 0) {
-      splitArr.push(array.slice(i*subListLength, i*subListLength+subListLength));
-    }
-  }
-  
-  return splitArr;
-};
-
-var getStateAbbrev = function(stateStr) {
-  var state = stateStr.toLowerCase();
-  var statesLength = states.length;
-  var i;
-
-  for (i=0; i<statesLength; i++) {
-    if (state === states[i].name.toLowerCase() || state === states[i].abbr.toLowerCase()) {
-      return states[i].abbr.toLowerCase();
-    }
-  }
-  return false;
-};
-
-var getFormattedDate = function(dateStr) {
-  var date = new Date(dateStr);
-  var month = date.getMonth();
-  var day = date.getDate();
-  var year = date.getFullYear();
-  var hours = date.getHours();
-  var mins = date.getMinutes();
-  
-  return month + '/' + day + '/' + year;// + ' ' + hours + ':' + mins;
-};
-
-var getPollData = function(params, callback) {
-  pollster.charts(params, function(resp) {
-    var data = [];
-    var respLength = resp.length;
-    var i;
-
-    for (i=0; i<respLength; i++) {
-      var item = {};
-      var choiceLength = resp[i].estimates.length;
-      var j;
-
-      if (resp[i].estimates.length > 0 && !isPrimary(resp[i])) {
-        item.title = resp[i].title;
-        item.url = resp[i].url;
-        item.raw_last_updated = resp[i].last_updated;
-        item.formatted_last_updated = getFormattedDate(item.raw_last_updated);
-        item.slug = resp[i].slug;
-        item.estimates = getFormattedChoices(resp[i].estimates);
-        data.push(item);
-      }
-    }
-
-    callback(data);
-  });
-};
-
-var order = function(a, b, desc) {
-  return desc ? b.value - a.value : a.value - b.value;
-};
-
-var descendingEstimates = function(a, b) {
-  return order(a, b, true);
-};
-
-var isPrimary = function(chart) {
-  var title = chart.title.toLowerCase();
-
-  return title.indexOf('primary') !== -1
-};
-
-var getFormattedChoices = function(choices) {
-  var i;
-  var choiceLength = choices.length;
-  var choiceArray = [];
-  
-  for (i=0; i<choiceLength; i++) {
-    choiceArray.push(getFormattedChoice(choices[i]));
-  }
-
-  return choiceArray.sort(descendingEstimates);
-};
-
-var getFormattedChoice = function(choice) {
-  var choiceObj = {};
-
-  choiceObj.formatted_name = getFormattedName(choice);
-  choiceObj.value = choice.value;
-  choiceObj.integer_value = Math.ceil(choice.value);
-  choiceObj.party = getFormattedParty(choice.party);
-  choiceObj.lower_party = choiceObj.party !== null ? choiceObj.party.toLowerCase() : 'null';
-
-  return choiceObj;
-};
-
-var getFormattedParty = function(party) {
-  var partyName;
-  var lowerParty = party !== null ? party.toLowerCase() : '';
-
-  if (lowerParty.indexOf('dem') !== -1) {
-    partyName = "Democrat";
-  } else if (lowerParty.indexOf('rep') !== -1) {
-    partyName = "Republican";
-  } else {
-    partyName = party;
-  }
-
-  return partyName;
-};
-
-var getFormattedName = function(choice) {
-  var formattedName;
-
-  if (choice.first_name && choice.last_name) {
-    formattedName = choice.first_name + ' ' + choice.last_name;
-  } else {
-    formattedName = choice.choice;
-  }
-
-  return formattedName;
 };
